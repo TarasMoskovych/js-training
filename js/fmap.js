b99c5eddd5b76d4d6f3adff67e4819870b591c2e
Taras Moskovych <moskovych.taras@gmail.com>
2018-06-09 16:08:33 +0300

    add fmap part2

--- js/fmap.js
+++ js/fmap.js
@@ -1,16 +1,6 @@
 function fmap(a, gen) {
     return function newGen() {
-        return a(gen());
-    }
-}
-
-
-function sequence(start, step) {
-    var start = start || 0;
-    var step = step || 1;
-
-    return function generator() {
-        return (start += step) - step;
+        return a(gen.apply(null, arguments));
     }
 }
 
@@ -22,7 +12,20 @@ function square(x) {
 
 var squareGen = fmap(square, gen);
 
+console.log("> Test fmap1");
+
 console.log(squareGen()); // 1
 console.log(squareGen()); // 4
 console.log(squareGen()); // 9
 console.log(squareGen()); // 16
+
+function add(a, b) {
+    return a + b;
+}
+
+var squareAdd = fmap(square, add);
+
+console.log("> Test fmap2");
+
+console.log(squareAdd(2, 3)); // 25 = (2 + 3) ^ 2
+console.log(squareAdd(5, 7)); // 144 = (5 + 7) ^ 2
